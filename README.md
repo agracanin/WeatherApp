2087f447e59aac2d7f005b3cf64abac8db971af7
Alen <alengracanin@gmail.com>
2023-03-27 16:16:21 -0400

    Comments for readability, config file added, README updated

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# WeatherApp
+# Weather App
+
+This is a simple weather app built with Python using the Tkinter library for the graphical user interface, the Pillow library for image processing, and the requests library for accessing OpenWeatherMap's API. The app allows users to enter a city name and displays the current weather information for that city.
+
+## Features
+The app displays the following weather information for the entered city:
+
+- Location: City, Country
+- Current weather icon
+- Current temperature in Fahrenheit
+- Description of current weather conditions
+
+## Demo
+![main](/Demo/standby.png)
+![utica](/Demo/utica.png)
+![tokyo](/Demo/tokyo.png)
+![error](/Demo/error.png)
+
+## Usage
+
+To use the app, follow these steps:
+
+1. Clone the repository to your local machine
+
+2. Obtain an API key from [OpenWeatherMap](https://openweathermap.org/api)
+
+3. Paste the API key into the config.ini file after "key="
+
+4. Navigate to the project and run the command 'python weather_app.py' in your terminal
+
+5. Enter a city name and click "Search Weather" button to display the current weather information.
+
+## Skills Demonstrated:
+
+- Proficiency in Python programming language
+
+- Experience with API integration using the requests library
+
+- Familiarity with working with external configuration files (configparser)
+
+- Experience with image processing using the Pillow library
+
+- Familiarity with developing graphical user interfaces using the Tkinter library
+
+- Experience with error handling and displaying error messages to users using the messagebox module
+
+- Ability to follow documentation and use external resources to solve programming challenges.
--- weather_app.py
+++ weather_app.py
@@ -8,27 +8,37 @@ import requests
 config_file = 'config.ini'
 config = ConfigParser()
 config.read(config_file)
+# Reading API key from config.ini
 api_key = config['api_key']['key']
 
+# Function to get weather information for a given city using OpenWeatherMap API
 def get_weather(city):
+    # Making a request to OpenWeatherMap API
     result = requests.get(
         f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&APPID={api_key}")
+    # Checking if the request was successful
     if result:
+        # Converting response to JSON
         json = result.json()
-        # (City, country, temp, temp_farenheight, icon, weather
+        # Extracting relevant data from JSON response
         city = json['name']
         country = json ['sys']['country']
         temp = round(json['main']['temp'])
         icon = json ['weather'][0]['icon']
         weather = json['weather'][0]['description']
+        # Storing relevant data in a tuple and returning it
         final =(city, country, temp, icon, weather)
         return final
     else:
+        # If request was not successful, return None
         return None
 
+# Search button function
 def search():
+    # Getting the city name entered by the user and storing in weather
     city = city_text.get()
     weather = get_weather(city)
+    # If weather information was obtained successfully, update all labels with stored data
     if weather:
         location_label['text'] = '{}, {}'.format(weather[0].upper(), weather[1])
         icon_path = 'weather_icons/{}.png'.format(weather[3])
@@ -39,33 +49,41 @@ def search():
         temp_label['text'] = '{}°F'.format(weather[2])
         weather_label['text'] = weather[4].upper()
     else:
+        # Displaying an error message if weather information could not be obtained for the entered city
         messagebox.showerror("Error", "City {} was not found".format(city))
 
+# Creating the main application window
 app = Tk()
 app.configure(background='#e5e5e5')
-
 app.title ("Weather App")
 app.geometry('450x350')
 
+# Heading/Title
 heading_title = Label(app, text='THE WEATHER WIZARD', font=("Impact", 18), fg="#14213d", bg="#e5e5e5")
 heading_title.pack(pady=(5,5))
 
+# Creating the city entry field
 city_text = StringVar()
 city_entry = Entry(app, textvariable=city_text, width=25, bg="#ffffff")
 city_entry.pack()
 
+# Creating the search button
 search_btn = Button(app, text="Search Weather", width=12, command=search, fg="#e5e5e5", bg="#14213d")
 search_btn.pack(pady=(5,10))
 
+# Location display
 location_label = Label(app, text="", font=('arial',20, 'bold','underline'), bg="#e5e5e5", fg="#14213d")
 location_label.pack(pady=(5,5))
 
+# Image of weather
 image = Label(app, bitmap='', bg="#e5e5e5")
 image.pack()
 
+# Temperatue display
 temp_label = Label (app, text='', font=('bold'), bg="#e5e5e5", fg="#fca311")
 temp_label.pack()
 
+# Weather description display
 weather_label = Label(app,text="", fg="#14213d", bg="#e5e5e5")
 weather_label.pack()
 
